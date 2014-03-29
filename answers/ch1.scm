25e28082d4791d20e1c4e44764b04132e4f2e689
Michiel van Oosten <mail@axion5.net>
2014-03-29 13:26:02 +1000

    update

--- answers/ch1.scm
+++ answers/ch1.scm
@@ -158,85 +158,101 @@
 (+ 0 9)
 
 ;; ;Ex1.10
-;; (define (A x y)
-;;   (cond ((= y 0) 0)
-;;         ((= x 0) (* 2 y))
-;;         ((= y 1) 2)
-;;         (else (A (- x 1)
-;;                  (A x (- y 1))))))
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+(A 1 10) ; 1024
+(A 2 4) ; 65536
+(A 3 3) ; 65536
 
-;; (define (f n) (A 0 n))
-;; (A 0 1) ;2
-;; (A 0 2) ;4 etc so:
-;; (define (fm n) (* 2 n))
-;; ;the value is doubled
-;; (define (g n) (A 1 n))
-;; (A 1 1) ; 2  2^1
-;; (A 1 2) 
-;;     (A 0 (A 1 1))
-;;     (A 0 2) ; 4  2^2
-;; (A 1 3) 
+(define (f n) (A 0 n))
+(A 0 1) ;2
+(f 1)
+(A 0 2) ;4 etc so:
+(A 0 3) ;6 etc so:
+(A 0 4) ;16 etc so:
+(define (fm n) (* 2 n)) ; == (A 0 n)
+;the value is doubled
+
+(define (g n) (A 1 n))
+(A 1 1) ; 2  2^1
+(A 1 2) 
+    (A 0 (A 1 1))
+    (A 0 2) ; 4  2^2
+(A 1 3) 
 ;;     (A 0 (A 1 2));8  2^3 etc so:
-;; (define (gm n) (expt 2 n))
-;; ;the exponent of 2 is increasing linearly.
-;; (define (h n) (A 2 n))
-;; (A 2 1) ;2 
-;; (A 2 2) ;=
-;; (A 1 (A 2 1)) 
-;; (A 1 2)
-;; (A 0 (A 1 1))
-;; (A 0 2) ;4 2^2 
-;; (A 2 3) ;= 
-;; (A 1 (A 2 2));16 2^2^2 
-;; (A 2 4) ;=65536 2^2^2^2
-;; (A 2 5) ;=2^65536 = 2^2^2^2^2
-;; ;mathematical notation is 2^^n I think.
-;; ;the number of exponents is increasing linearly. 
-;; (A 3 1) ; 2
-;; (A 3 2) ; 2^2
-;; (A 3 3) ; 2^2^2^2
-;; ;the number of exponents is doubling 2^^(2^(n-1))
+(define (gm n) (expt 2 n)) == (A 1 n)
+;the exponent of 2 is increasing linearly.
+
+(define (h n) (A 2 n))
+(A 2 1) ;2 
+
+(A 2 2) ;=
+(A 1 (A 2 1)) 
+(A 1 2)
+(A 0 (A 1 1))
+(A 0 2) ;4 2^2 
+
+(A 2 3) ;= 
+(A 1 (A 2 2));16 2^2^2 
+
+(A 2 4) ;=65536 2^2^2^2
+
+(A 2 5) ;=2^65536 = 2^2^2^2^2
+;mathematical notation is 2^^n I think.
+;the number of exponents is increasing linearly. 
+(A 3 1) ; 2
+(A 3 2) ; 2^2
+(A 3 3) ; 2^2^2^2
+;the number of exponents is doubling 2^^(2^(n-1))
+
+;; Ex.1.11
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(count-change 1000)
+;recursive
+(define (f n)
+  (cond ((< n 3) n)
+        ((>= n 3)  
+         (+ (* 1 (f (- n 1)))
+            (* 2 (f (- n 2)))
+            (* 3 (f (- n 3)))
+            ))))
+;iterative
+(define (fi n)
+  (cond ((< n 3) n)
+        (else (f-iterate 0 1 2 (- n 3)))))
+
+(define (f-iterate n0 n1 n2 counter)
+  (define n (+ (* 1  n2) 
+               (* 2  n1) 
+               (* 3  n0)))
+  (cond ((> counter 0) (f-iterate n1 n2 n (- counter 1)))
+         (else n)))
+(f 5);25
+(fi 5);25
 
 
-;; (define (count-change amount)
-;;   (cc amount 5))
-
-;; (define (cc amount kinds-of-coins)
-;;   (cond ((= amount 0) 1)
-;;         ((or (< amount 0) (= kinds-of-coins 0)) 0)
-;;         (else (+ (cc amount
-;;                      (- kinds-of-coins 1))
-;;                  (cc (- amount
-;;                         (first-denomination kinds-of-coins))
-;;                      kinds-of-coins)))))
-
-;; (define (first-denomination kinds-of-coins)
-;;   (cond ((= kinds-of-coins 1) 1)
-;;         ((= kinds-of-coins 2) 5)
-;;         ((= kinds-of-coins 3) 10)
-;;         ((= kinds-of-coins 4) 25)
-;;         ((= kinds-of-coins 5) 50)))
-
-;; ;Ex1.11
-;; ;recursive
-;; (define (f n)
-;;   (cond ((< n 3) n)
-;;         ((>= n 3)  
-;;          (+ (* 1 (f (- n 1)))
-;;             (* 2 (f (- n 2)))
-;;             (* 3 (f (- n 3)))
-;;             ))))
-;; ;iterative
-;; (define (fi n)
-;;   (cond ((< n 3) n)
-;;         (else (f-iterate 0 1 2 (- n 3)))))
-
-;; (define (f-iterate n0 n1 n2 counter)
-;;   (define n (+ (* 1  n2) 
-;;                (* 2  n1) 
-;;                (* 3  n0)))
-;;   (cond ((> counter 0) (f-iterate n1 n2 n (- counter 1)))
-;;          (else n)))
 ;; ;Ex1.12
 ;; (define (pt r n)
 ;;   (cond
